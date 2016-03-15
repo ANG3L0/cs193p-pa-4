a2babd9e8ab53d3a783b79f8a52bb6df1f942888
Ugghhh <angelowo@umich.edu>
2016-03-15 11:31:34 -0700

    Finish task of highlighting keywords

--- Smashtag/TweetTableViewCell.swift
+++ Smashtag/TweetTableViewCell.swift
@@ -16,44 +16,92 @@ class TweetTableViewCell: UITableViewCell {
         }
     }
     
+    private struct Color {
+        static let red = UIColor.redColor()
+        static let brown = UIColor.brownColor()
+        static let blue = UIColor.blueColor()
+    }
+    
     @IBOutlet weak var tweetProfileImageView: UIImageView!
     @IBOutlet weak var tweetScreenNameLabel: UILabel!
     @IBOutlet weak var tweetTextLabel: UILabel!
+    
+    @IBOutlet weak var tweetCreatedLabel: UILabel!
+    
+    let qos = Int(QOS_CLASS_USER_INITIATED.rawValue)
     
     func updateUI() {
         // reset any existing tweet information
         tweetTextLabel?.attributedText = nil
         tweetScreenNameLabel?.text = nil
         tweetProfileImageView?.image = nil
-//        tweetCreatedLabel?.text = nil
+        tweetCreatedLabel?.text = nil
         
         // load new information from our tweet (if any)
         if let tweet = self.tweet
         {
+
             tweetTextLabel?.text = tweet.text
+            
             if tweetTextLabel?.text != nil  {
                 for _ in tweet.media {
                     tweetTextLabel.text! += " 📷"
                 }
+                highlightIndicators(tweetTextLabel, hashtagColor: Color.brown, screennameColor: Color.red, urlColor:  Color.blue)
             }
+            
+            
             
             tweetScreenNameLabel?.text = "\(tweet.user)" // tweet.user.description
             
             //TODO need to fix this
             if let profileImageURL = tweet.user.profileImageURL {
-                if let imageData = NSData(contentsOfURL: profileImageURL) { // blocks main thread!
-                    tweetProfileImageView?.image = UIImage(data: imageData)
+                dispatch_async(dispatch_get_global_queue(qos, 0)) { () -> Void in
+                    if let imageData = NSData(contentsOfURL: profileImageURL) {
+                        dispatch_async(dispatch_get_main_queue()) { () -> Void in
+                            self.tweetProfileImageView?.image = UIImage(data: imageData)
+                        }
+                    }
                 }
             }
             
-//            let formatter = NSDateFormatter()
-//            if NSDate().timeIntervalSinceDate(tweet.created) > 24*60*60 {
-//                formatter.dateStyle = NSDateFormatterStyle.ShortStyle
-//            } else {
-//                formatter.timeStyle = NSDateFormatterStyle.ShortStyle
-//            }
-//            tweetCreatedLabel?.text = formatter.stringFromDate(tweet.created)
+            let formatter = NSDateFormatter()
+            if NSDate().timeIntervalSinceDate(tweet.created) > 24*60*60 {
+                formatter.dateStyle = NSDateFormatterStyle.ShortStyle
+            } else {
+                formatter.timeStyle = NSDateFormatterStyle.ShortStyle
+            }
+            tweetCreatedLabel?.text = formatter.stringFromDate(tweet.created)
         }
 
     }
+    // MARK: - Color highlight Extension
+    private func highlightIndicators(label: UILabel, hashtagColor: UIColor, screennameColor: UIColor, urlColor: UIColor) {
+        let ht = tweet!.hashtags
+        let urls = tweet!.urls
+        let mentions = tweet!.userMentions
+        let attributedText = NSMutableAttributedString(string: label.text!)
+        for i in 0..<ht.count {
+            attributedText.addAttribute(NSForegroundColorAttributeName, value: hashtagColor, range: ht[i].nsrange)
+        }
+        for i in 0..<urls.count {
+            attributedText.addAttribute(NSForegroundColorAttributeName, value: urlColor, range: urls[i].nsrange)
+        }
+        for i in 0..<mentions.count {
+            attributedText.addAttribute(NSForegroundColorAttributeName, value: screennameColor, range: mentions[i].nsrange)
+        }
+        label.attributedText = attributedText
+    }
 }
+
+
+
+//private func getIndexTuple(keywordArray: IndexedKeyword) -> (start: Int, end: Int) {
+//    let stringTuple = keywordArray.description.characters.split{$0 == "("}.map(String.init)[1]
+//    var stringNum = stringTuple.characters.split{$0 == ","}.map(String.init)
+//    stringNum[1].removeAtIndex(stringNum[1].endIndex.predecessor())
+//    stringNum[1].removeAtIndex(stringNum[1].startIndex)
+//    let start = Int(stringNum[0])!
+//    let end = Int(stringNum[1])!
+//    return (start,end)
+//}
